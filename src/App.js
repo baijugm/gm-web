4af0c41f2f951a62eae737cca8324fa197ca001a
baijugm <baiju@goodmeetings.ai>
2021-12-09 11:24:33 +0530

    image replace

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import "./assets/css/common.css";
 import "./assets/css/simplegrid.css";
-import image1 from "./assets/images/1.png";
+import image1 from "./assets/images/1.jpg";
 import image2 from "./assets/images/2.jpg";
 import image3 from "./assets/images/3.jpg";
 import image4 from "./assets/images/4.jpg";
